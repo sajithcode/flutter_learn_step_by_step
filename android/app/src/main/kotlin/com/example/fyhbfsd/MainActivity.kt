66ef3922efd6580d6f9f6c3bf19aeb542721d5c7
Sajith Rathna Sampath <sajithsampath721@gmail.com>
2025-09-23 07:47:33 +0530

    Add Windows runner files for Flutter application
    - Created runner.exe.manifest to define application settings and compatibility for Windows 10 and 11.
    - Implemented utils.cpp and utils.h for console creation, command line argument retrieval, and UTF-16 to UTF-8 string conversion.
    - Developed win32_window.cpp and win32_window.h to manage a high DPI-aware Win32 window, including window creation, message handling, and theme updates.

---
+++ android/app/src/main/kotlin/com/example/fyhbfsd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fyhbfsd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text("Code With leo"),
+      backgroundColor: Colors.brown[700],
+      centerTitle: true,
+    ),
+    body: const Home(),
+  )));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello!");
+  }
+}
